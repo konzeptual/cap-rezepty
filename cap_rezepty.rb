1ba8db108f59ff926d9c7fbab67f19b58d978643
krick <antipin.konstantin@googlemail.com>
2010-02-20 22:48:07 +0300

    Added main file

---
+++ cap_rezepty.rb
@@ -0,0 +1,8 @@
+# ;lj
+if respond_to?(:namespace) do 
+    load File.join(File.dirname(__FILE__), 'utils.rb') 
+    load File.join(File.dirname(__FILE__), 'web-server.rb') 
+    load File.join(File.dirname(__FILE__), 'db.rb') 
+    load File.join(File.dirname(__FILE__), 'settings.rb') 
+    load File.join(File.dirname(__FILE__), 'shared_dirs.rb') 
+  end
