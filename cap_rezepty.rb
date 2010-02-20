6bb996f1f2a7a0b7f47d872ad987c2f5030f25cb
krick <antipin.konstantin@googlemail.com>
2010-02-20 23:01:24 +0300

    fix path to web_server.rb

--- cap_rezepty.rb
+++ cap_rezepty.rb
@@ -1,7 +1,7 @@
 # ;lj
 if (respond_to?(:namespace)) 
   load File.join(File.dirname(__FILE__), 'utils.rb') 
-  load File.join(File.dirname(__FILE__), 'web-server.rb') 
+  load File.join(File.dirname(__FILE__), 'web_server.rb') 
   load File.join(File.dirname(__FILE__), 'db.rb') 
   load File.join(File.dirname(__FILE__), 'settings.rb') 
   load File.join(File.dirname(__FILE__), 'shared_dirs.rb') 
